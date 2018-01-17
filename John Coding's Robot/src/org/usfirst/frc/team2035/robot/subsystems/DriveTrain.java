7b7176b9797174acfe508782b2431596d6e51e23
John Coding <tcarr@cusd.me>
2018-01-16 16:47:18 -0800

    Attempt #2

--- John Coding's Robot/src/org/usfirst/frc/team2035/robot/subsystems/DriveTrain.java
+++ John Coding's Robot/src/org/usfirst/frc/team2035/robot/subsystems/DriveTrain.java
@@ -10,14 +10,69 @@ package org.usfirst.frc.team2035.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- * An example subsystem.  You can replace me with your own Subsystem.
+ * Hooray For Drive Train! 10 Points to John Coding!
+ * (Of course I didn't copy paste the entire thing!)
  */
 public class DriveTrain extends Subsystem {
-	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
+	private static Joystick stick;
+	private SpeedController motorFrontLeft;
+	private SpeedController motorBackLeft;
+	private SpeedController motorFrontRight;
+	private SpeedController motorBackRight;
+	private DoubleSolenoid gearShifter;
+	private RobotDrive drive;
 
-	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
+	/**
+	 * Creates an new DriveTrain subsystem<br>
+	 * Initializes 4 wpilib Victors for motors, a Joystick, DoubleSolenoid, and RobotDrive object
+	 */
+	public DriveTrain() {
+		super("Drive Train");
+		motorFrontLeft = new Victor(RobotMap.F_LEFT_MOTOR_PWM);  // initialize the motor as a victor on channel 0
+		motorBackLeft = new Victor(RobotMap.B_LEFT_MOTOR_PWM);
+		motorFrontRight = new Victor(RobotMap.F_RIGHT_MOTOR_PWM);
+		motorBackRight = new Victor(RobotMap.B_RIGHT_MOTOR_PWM);
+		stick = new Joystick(RobotMap.JOYSTICK_A_USB_NUMBER);	// initialize the joystick on USB 0
+		drive = new RobotDrive(motorFrontLeft,motorBackLeft,motorFrontRight,motorBackRight);
+		gearShifter = new DoubleSolenoid(RobotMap.PCM_ID_DRIVER, RobotMap.HIGH_GEAR, RobotMap.LOW_GEAR);
 	}
+
+
+	protected void initDefaultCommand()
+	{
+		//super.setDefaultCommand(new StandardDrive(drive, stick));
+	}
+
+	/**
+	 * Drives the robot with a joystick
+	 */
+	public void arcadeDrive() {
+		drive.arcadeDrive(stick.getY(), (stick.getX()));
+	}
+
+	/**
+	 * Drives the robot at a certain speed and curve
+	 * @param speed
+	 * @param curve
+	 */
+	public void drive(double speed, double curve) {
+		drive.drive(speed, curve);
+	}
+
+	/**
+	 * Shifts the drive train into low geara
+	 */
+	public void shiftLowGear() {
+		System.out.println("Gotta Go LOW");
+		gearShifter.set(DoubleSolenoid.Value.kReverse);
+	}	
+	
+	/**
+	 * Shifts the drive train into high gear
+	 */
+	public void shiftHighGear() {
+		System.out.println("Gotta Go HIGH");
+		gearShifter.set(DoubleSolenoid.Value.kForward);
+	}
+	
 }
