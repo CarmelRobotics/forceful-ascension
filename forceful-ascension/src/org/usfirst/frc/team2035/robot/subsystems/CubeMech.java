bec5607362dfe14aa2bc5bcdfb45dc0a98457d4f
Aristotle Henderson <aristotleinternet@gmail.com>
2018-01-20 16:38:07 -0800

    Set up new project in Eclipse and replaced files; begun drivetrain

---
+++ forceful-ascension/src/org/usfirst/frc/team2035/robot/subsystems/CubeMech.java
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team2035.robot.subsystems;
+
+import org.usfirst.frc.team2035.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Controls the cube intake and carrying mechanism.
+ */
+public class CubeMech extends Subsystem {
+	
+	private Victor rollerR; //right side cube mechanism roller
+	private Victor rollerL; //left side cube mechanism roller
+	
+	public CubeMech() {
+		//sets roller variables to pins on Roborio
+		rollerR = new Victor(RobotMap.R_CUBEMECH_MOTOR_PWM);
+		rollerL = new Victor(RobotMap.L_CUBEMECH_MOTOR_PWM);
+	}
+
+	/**
+	 * Rolls cubes into mechanism
+	 */
+	public void rollIn() {
+		//sets cube mech motors to positive speed
+		rollerR.set(RobotMap.CUBEMECH_IN_SPEED);
+		rollerL.set(RobotMap.CUBEMECH_IN_SPEED);
+	}
+	
+	/**
+	 * Rolls cubes out of mechanism
+	 */
+	public void rollOut() {
+		//sets cube mech motors to negative speed
+		rollerR.set(RobotMap.CUBEMECH_OUT_SPEED);
+		rollerL.set(RobotMap.CUBEMECH_OUT_SPEED);
+	}
+	
+	/**
+	 * Stops rollers from moving
+	 */
+	public void stopRoll() {
+		//stops cube mech motors
+		rollerR.set(0.0);
+		rollerL.set(0.0);
+	}
+	
+	public void initDefaultCommand() {
+		
+	}
+}
---
+++ forceful-ascension/src/org/usfirst/frc/team2035/robot/subsystems/Wings.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team2035.robot.subsystems;
+
+import org.usfirst.frc.team2035.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *Controls the wing management system.
+ */
+public class Wings extends Subsystem {
+	
+	private Solenoid wingR; //right side solenoid keeping wings up
+	private Solenoid wingL; //left side solenoid keeping wings up
+	
+	public Wings() {
+		wingR = new Solenoid(RobotMap.R_WINGS);
+		wingL = new Solenoid(RobotMap.L_WINGS);
+	}
+
+	//turns wing solenoids on, keeping the wings up
+	public void wingsSolenoidsOn() {
+		wingR.set(true);
+		wingL.set(true);
+    }
+    
+  //turns wing solenoids off, dropping the wings
+	public void wingsSolenoidsOff() {
+		wingR.set(false);
+		wingL.set(false);
+    }
+    
+	public void initDefaultCommand() {
+		
+	}
+
+}
