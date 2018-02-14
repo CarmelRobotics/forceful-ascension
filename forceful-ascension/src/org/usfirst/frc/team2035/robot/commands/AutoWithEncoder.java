1bf892541cdc5f21a960999360c1a648bf8e6412
Fang Tao <fanghtao@gmail.com>
2018-02-13 17:50:28 -0800

    made the merge work

--- forceful-ascension/src/org/usfirst/frc/team2035/robot/commands/AutoWithEncoder.java
+++ forceful-ascension/src/org/usfirst/frc/team2035/robot/commands/AutoWithEncoder.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team2035.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team2035.robot.OI;
 import org.usfirst.frc.team2035.robot.subsystems.*;
